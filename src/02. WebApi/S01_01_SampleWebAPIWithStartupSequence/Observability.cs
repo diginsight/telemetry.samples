e4420829e3353e24f67b2f579365e3d0c0c109d9
Dario Airoldi <darioa@microsoft.com>
2025-11-07 07:06:17 -0800

    fix

--- src/02. WebApi/S01_01_SampleWebAPIWithStartupSequence/Observability.cs
+++ src/02. WebApi/S01_01_SampleWebAPIWithStartupSequence/Observability.cs
@@ -1,3 +1,4 @@
+using Diginsight.Diagnostics;
 using System.Diagnostics;
 using System.Reflection;
 
@@ -7,5 +8,6 @@ internal static class Observability
 {
     public static readonly ActivitySource ActivitySource = new(Assembly.GetExecutingAssembly().GetName().Name!);
     public static ILoggerFactory LoggerFactory { get; set; }
+    static Observability() => ObservabilityRegistry.RegisterComponent(factory => LoggerFactory = factory);
 }
 
--- src/02. WebApi/S01_01_SampleWebAPIWithStartupSequence/Program.cs
+++ src/02. WebApi/S01_01_SampleWebAPIWithStartupSequence/Program.cs
@@ -11,11 +11,9 @@ public class Program
     public static void Main(string[] args)
     {
         using var observabilityManager = new ObservabilityManager();
-        Observability.LoggerFactory = observabilityManager.LoggerFactory;
         ObservabilityRegistry.RegisterLoggerFactory(observabilityManager.LoggerFactory);
 
         ILogger logger = observabilityManager.LoggerFactory.CreateLogger(typeof(Program));
-
 
         WebApplication app;
         using (var activity = Observability.ActivitySource.StartMethodActivity(logger, new { args }))
